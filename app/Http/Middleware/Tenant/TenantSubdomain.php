38a167d41e9644a24d1757c52d67e31fc2aedccd
Pablodomingos <pablodomingos1700@gmail.com>
2023-06-27 22:06:18 -0300

    Refactor: adjusted file.

--- app/Http/Middleware/Tenant/TenantSubdomain.php
+++ app/Http/Middleware/Tenant/TenantSubdomain.php
@@ -3,23 +3,21 @@
 namespace App\Http\Middleware\Tenant;
 
 use App\Exceptions\NotFoundCustomException;
-use App\Models\Tenant;
+use App\Tenant\ManagerTenant;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Illuminate\Support\Str;
 
 class TenantSubdomain
 {
     public function handle(Request $request, Closure $next): Response
     {
-        $host = Str::of($request->getHost())->whenContains('.', fn (string $host) => Str::before($host, '.'));
-        $tenant = Tenant::where('subdomain', '=', $host)->first();
+        $tenant = app(ManagerTenant::class)->tenant();
 
         if ($tenant) {
             return $next($request);
         }
 
-        throw new NotFoundCustomException(trans('custom.host', ['attribute' => $host]));
+        throw new NotFoundCustomException(__('custom.host'));
     }
 }
