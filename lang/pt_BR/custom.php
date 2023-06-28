cddf8e272b61ae90eab20b564eb64cfa3e1cf30b
Pablodomingos <pablodomingos1700@gmail.com>
2023-06-27 22:12:27 -0300

    Refactor: adjusted message.

--- lang/pt_BR/custom.php
+++ lang/pt_BR/custom.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'host' => 'O domínio :attribute não está cadastrado na aplicação',
+    'host' => 'O domínio não está cadastrado na aplicação',
     'unauthorized_login' => 'Usuário ou senha inválidos'
 ];
